ccee2217658b16390a74dd14f1749dfd17941a67
Classics <maks.zablocky@gmail.com>
2023-06-30 22:46:53 +0300

    folder will be renamed

--- Lesson_3/Part_1/3_1_16.py
+++
@@ -1,5 +0,0 @@
-# импортируем тип date из модуля datetime
-from datetime import date
-
-# выводим текущую дату
-print(date.today())
--- Lesson_3/Part_1/3_1_20.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-
-
-def get_min_max(list_of_dates):
-    if not list_of_dates:
-        return ()
-    return min(list_of_dates), max(list_of_dates)
