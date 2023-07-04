a805731ff456df93b09fa0c4b062da76e23dfe01
Classics <maks.zablocky@gmail.com>
2023-07-04 22:21:20 +0300

    new file files.txt

--- Lesson_2/Part_2/2_2_9.py
+++ Lesson_2/Part_2/2_2_9.py
@@ -1,6 +1,6 @@
 with open('files.txt', encoding='utf-8') as f:
     lst = f.readlines()
-    dict_files
+    dict_files = {}
     for i in lst:
         if i[-3:] not in dict_files.keys():
             dict_files[i[-3:]] = i
