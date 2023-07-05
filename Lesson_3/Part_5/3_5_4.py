6a37100692a4fe61b41763a10a3799f65098b0c4
Classics <maks.zablocky@gmail.com>
2023-07-05 21:36:01 +0300

    changes

--- Lesson_3/Part_5/3_5_4.py
+++ Lesson_3/Part_5/3_5_4.py
@@ -4,9 +4,9 @@ a = datetime.strptime(input(), '%d.%m.%Y')
 b = datetime.strptime(input(), '%d.%m.%Y')
 
 start = False
-add=1
+add = 1
 while a <= b:
-    if (a.day+a.month)%2 == 1:
+    if (a.day + a.month) % 2 == 1:
         start = True
         add = 3
     if start:
