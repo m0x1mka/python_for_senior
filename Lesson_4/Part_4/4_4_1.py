6a46f368cf06e4e9523bea6c50c72bb24b791dc4
Classics <maks.zablocky@gmail.com>
2023-07-20 22:04:18 +0300

    new files 4_4_21.py

---
+++ Lesson_4/Part_4/4_4_1.py
@@ -0,0 +1,9 @@
+import json
+
+countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+             'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+             'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+             'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+
+answer = json.dumps(countries, separators=(",", " - "), indent=3, sort_keys=True)
+print(answer)
