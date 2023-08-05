e8f5f7ab76b1170abc8ca7b96f82200af51bcfcc
Classics <maks.zablocky@gmail.com>
2023-08-05 14:54:05 +0300

    new file 4_5_17.py and .zip

---
+++ Lesson_4/Part_5/4_5_17.py
@@ -0,0 +1,15 @@
+from zipfile import ZipFile
+from datetime import datetime
+
+with ZipFile("4_5_17_data.zip") as zip_data:
+    lst = []
+    for i in ZipFile.infolist(zip_data):
+        a = i.date_time
+        if datetime.strptime(f"{a[0]}-{a[1]}-{a[2]} {a[3]}:{a[4]}:{a[5]}", "%Y-%m-%d %H:%M:%S") > datetime.strptime(
+                "2021-11-30 14:22:00",
+                "%Y-%m-%d %H:%M:%S"):
+            lst.append(i)
+    for i in sorted(lst, key=lambda x: x.filename):
+        if not i.is_dir():
+            ans = i.filename
+            print(ans[ans.index("/")+1:] if "/" in ans else ans)
