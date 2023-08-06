1d558876eb6cce913081d8247152e015c5c3394b
m0nday <maks.zablocky@gmail.com>
2023-08-06 13:43:32 +0300

    new files 4_5_20.py and .zip

--- Lesson_4/Part_5/4_5_19 and 20/4_5_20.py
+++ Lesson_4/Part_5/4_5_19 and 20/4_5_20.py
@@ -1,13 +1,11 @@
 from zipfile import ZipFile
+import os.path
 
 file_names = ['how to prove.pdf', 'fipi_demo_2022.pdf', 'Hollow Knight Silksong.exe',
               'code.jpeg', 'stepik.png', 'readme.txt', 'shopping_list.txt',
               'Alexandra Savior – Crying All the Time.mp3', 'homework.py', 'test.py']
 
-for i in file_names:
-    with open(i, "w") as f:
-        pass
-
-with ZipFile("4_5_19_answer.zip", mode="w") as zip_data:
+with ZipFile("4_5_20_answer.zip", mode="w") as zip_w:
     for i in file_names:
-        zip_data.write(i)
+        if os.path.getsize(i) <= 100:
+            zip_w.write(i)
