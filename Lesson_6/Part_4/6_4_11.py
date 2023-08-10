cb555c2ba6eab5380b07731ed18ccf32bb5c0367
m0nday <maks.zablocky@gmail.com>
2023-08-10 10:06:03 +0300

    new file 6_4_11.py

---
+++ Lesson_6/Part_4/6_4_11.py
@@ -0,0 +1,28 @@
+from collections import namedtuple
+
+User = namedtuple('User', ['name', 'surname', 'email', 'plan'])
+
+users = [User('Mary', 'Griffin', 'sonnen@yahoo.com', 'Basic'),
+         User('Brenda', 'Young', 'retoh@outlook.com', 'Silver'),
+         User('Kathleen', 'Lyons', 'balchen@att.net', 'Gold'),
+         User('Pamela', 'Hicks', 'corrada@sbcglobal.net', 'Silver'),
+         User('William', 'Townsend', 'kosact@verizon.net', 'Gold'),
+         User('Clayton', 'Morris', 'berserk@yahoo.com', 'Silver'),
+         User('Dorothy', 'Dennis', 'sequin@live.com', 'Gold'),
+         User('Tyler', 'Walker', 'noahb@comcast.net', 'Basic'),
+         User('Joseph', 'Moore', 'ylchang@sbcglobal.net', 'Silver'),
+         User('Kenneth', 'Richardson', 'tbusch@me.com', 'Bronze'),
+         User('Stephanie', 'Bush', 'neuffer@live.com', 'Gold'),
+         User('Gregory', 'Hughes', 'juliano@att.net', 'Basic'),
+         User('Tracy', 'Wallace', 'sblack@me.com', 'Silver'),
+         User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
+         User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
+
+subscription = ["Basic", "Bronze", "Silver", "Gold"]
+
+name_sorted = sorted(users, key=lambda x: x.email)
+sub_sorted = sorted(name_sorted, key=lambda x: subscription.index(x.plan), reverse=True)
+for i in sub_sorted:
+    print(f"{i.name} {i.surname}")
+    print(f"  Email: {i.email}")
+    print(f"  Plan: {i.plan}\n")
