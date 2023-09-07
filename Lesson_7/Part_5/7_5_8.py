b35c1e298c799d2e8515adb4cf3e7b7a15d598f9
m0nday <maks.zablocky@gmail.com>
2023-09-07 21:22:00 +0300

    new file 7_5_8.py

---
+++ Lesson_7/Part_5/7_5_8.py
@@ -0,0 +1,32 @@
+import string
+
+alphabet = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о",
+            "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
+
+
+class PasswordError(Exception):
+    pass
+
+
+class LengthError(PasswordError):
+    pass
+
+
+class LetterError(PasswordError):
+    pass
+
+
+class DigitError(PasswordError):
+    pass
+
+
+def is_good_password(password):
+    if len(password) < 9:
+        raise LengthError
+    elif not (any([i.isupper() for i in password]) and any(
+        [i.islower() for i in password])) or password.isdigit() or not any(
+        i in string.ascii_letters + "".join(alphabet) for i in password):
+        raise LetterError
+    elif not any([i.isdigit() for i in password]):
+        raise DigitError
+    return True
