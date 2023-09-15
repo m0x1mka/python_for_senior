968e083365d908a199c27c84ca1bd4c3198ce209
m0nday <maks.zablocky@gmail.com>
2023-09-15 09:42:20 +0300

    new file 9_1_14.py

---
+++ Lesson_9/Part_1/9_1_14.py
@@ -0,0 +1,6 @@
+names = ['Moana', 'Cars', 'Zootopia', 'Ratatouille', 'Coco', 'Inside Out', 'Finding Nemo', 'Frozen']
+budgets = [150000000, 120000000, 150000000, 150000000, 180000000, 175000000, 94000000, 150000000]
+box_offices = [643331111, 462216280, 1023784195, 620702951, 807082196, 857611174, 940335536, 1280802282]
+
+for i in sorted(zip(names, budgets, box_offices), key=lambda x: x[0]):
+    print(f"{i[0]}: {i[2] - i[1]}$")
