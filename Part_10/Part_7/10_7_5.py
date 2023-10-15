de5428a7c9792d207505adcf8e8b45f2834c43e1
m0nday <maks.zablocky@gmail.com>
2023-10-15 10:21:37 +0300

    new file 10_7_5.py

---
+++ Part_10/Part_7/10_7_5.py
@@ -0,0 +1,38 @@
+from collections import namedtuple
+
+Person = namedtuple('Person', ['name', 'nationality', 'sex', 'birth', 'death'])
+
+persons = [Person('E. M. Ashe', 'American', 'male', 1867, 1941),
+           Person('Goran Aslin', 'Swedish', 'male', 1980, 0),
+           Person('Erik Gunnar Asplund', 'Swedish', 'male', 1885, 1940),
+           Person('Genevieve Asse', 'French', 'female', 1949, 0),
+           Person('Irene Adler', 'Swedish', 'female', 2005, 0),
+           Person('Sergio Asti', 'Italian', 'male', 1926, 0),
+           Person('Olof Backman', 'Swedish', 'male', 1999, 0),
+           Person('Alyson Hannigan', 'Swedish', 'female', 1940, 1987),
+           Person('Dana Atchley', 'American', 'female', 1941, 2000),
+           Person('Monika Andersson', 'Swedish', 'female', 1957, 0),
+           Person('Shura_Stone', 'Russian', 'male', 2000, 0),
+           Person('Jon Bale', 'Swedish', 'male', 2000, 0)]
+
+
+def find_sweden(itr):
+    for i in itr:
+        if i.nationality == "Swedish":
+            yield i
+
+
+def find_male(itr):
+    for i in itr:
+        if i.sex == "male":
+            yield i
+
+
+def find_alive(itr):
+    for i in itr:
+        if i.death == 0:
+            yield i
+
+
+print(max(find_alive(find_male(find_sweden(persons))), key=lambda x: x.birth).name)
+
